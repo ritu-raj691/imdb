ca75d6d87a31f211326c024869805d0885e09949
ritu-raj691 <rituraj691@gmail.com>
2023-09-15 01:25:12 +0530

    loading effect added

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex justify-center mt-20">
+      <img src="spinner.svg" alt="loading..." />
+    </div>
+  );
+};
+
+export default Loading;
